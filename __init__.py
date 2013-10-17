1718c6e4870780bfa37df59783a507164a0047fe
Iurii Kudriavtsev <iurii.kudriavtsev@gmail.com>
2013-10-17 21:31:51 +0200

    adding simple settings config.

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+import os
 
 
 app = Flask(__name__)
 app.debug = True
 
+if 'PERSONAL_APP_SETTINGS' in os.environ:
+    app.config.from_envvar('PERSONAL_APP_SETTINGS')
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
 
