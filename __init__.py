fce2ed1ea05e4625ed2f4b7a1c736a0081c3fcae
Iurii Kudriavtsev <iurii.kudriavtsev@gmail.com>
2013-10-17 22:06:17 +0200

    adding base linkedin API routine.

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from linkedin import linkedin
 import os
 
 
@@ -10,6 +11,16 @@ if 'PERSONAL_APP_SETTINGS' in os.environ:
 
 @app.route('/')
 def index():
+    authentication = linkedin.LinkedInDeveloperAuthentication(
+        app.config['API_KEY'],
+        app.config['API_SECRET'],
+        app.config['USER_TOKEN'],
+        app.config['USER_SECRET'],
+        request.base_url,
+        linkedin.PERMISSIONS.enums.values())
+    application = linkedin.LinkedInApplication(authentication)
+    profile = application.get_profile()
+    print profile
     return render_template('index.html')
 
 
