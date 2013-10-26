09d0728cfb583489ed2fb1f64b7969ceddfbc158
Iurii Kudriavtsev <iurii.kudriavtsev@gmail.com>
2013-10-26 12:49:09 +0200

    minor changes to wordings.

--- forms.py
+++ forms.py
@@ -8,7 +8,7 @@ class ShortMessageForm(Form):
     Form - send a short message.
     Has two fields - email and message.
     """
-    email = StringField("Email", description="It won't be printed.", validators=[DataRequired(message="This field is required."),
+    email = StringField("Email", description="So that I could email you back", validators=[DataRequired(message="This field is required."),
         Email(message="This does not look like a valid email address.")])
-    message = TextAreaField("Message", description="Maximum 300 characters.", validators=[DataRequired(message="This field is required."),
+    message = TextAreaField("Message", description="Maximum 300 characters", validators=[DataRequired(message="This field is required."),
         Length(max=300, message="The message can not be longer than 300 characters.")])
