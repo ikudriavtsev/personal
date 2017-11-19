04fbbc9cc355fa1269337c4a67a9148f63cee645
Iurii Kudriavtsev <iurii.kudriavtsev@gmail.com>
2017-11-19 16:30:29 +0100

    Adding LinkedIn projects to the PDF.

--- utils.py
+++ utils.py
@@ -10,13 +10,14 @@ import time
 
 # the following code is used to make datetime.now() be TZ aware
 # taken directly from the python docs: http://docs.python.org/2/library/datetime.html#tzinfo-objects
-STDOFFSET = timedelta(seconds = -time.timezone)
+STDOFFSET = timedelta(seconds=-time.timezone)
 if time.daylight:
-    DSTOFFSET = timedelta(seconds = -time.altzone)
+    DSTOFFSET = timedelta(seconds=-time.altzone)
 else:
     DSTOFFSET = STDOFFSET
 
 DSTDIFF = DSTOFFSET - STDOFFSET
+
 
 class LocalTimezone(tzinfo):
 
@@ -56,21 +57,20 @@ def compose_pdf(profile):
     HEADLINE_FONT_SIZE = 18
     PARAGRAPH_SPACE = 30
     BULLET_INDENT = 10
-    #FIXME (Iurii Kudriavtsev): image should be scaled appropriately
-    IMG_WIDTH = 3*cm
-    IMG_HEIGHT = 4*cm
+    # FIXME (Iurii Kudriavtsev): image should be scaled appropriately
+    IMG_WIDTH = 4 * cm
+    IMG_HEIGHT = 4 * cm
 
     doc = SimpleDocTemplate(
         output,
-        # pagesize=letter,
-        topMargin=2*cm,
+        topMargin=2 * cm,
         rightMargin=cm,
-        bottomMargin=2*cm,
+        bottomMargin=2 * cm,
         leftMargin=cm,
     )
 
     # define styles that will be used
-    styles=getSampleStyleSheet()
+    styles = getSampleStyleSheet()
     styles["Normal"].fontName = FONT_NAME
     styles["Normal"].fontSize = FONT_SIZE
     styles["Normal"].leading = LEADING
@@ -82,10 +82,10 @@ def compose_pdf(profile):
         alignment=TA_CENTER,
         fontName=FONT_NAME,
         fontSize=HEADLINE_FONT_SIZE,
-        leading = LEADING,
+        leading=LEADING,
         spaceBefore=PARAGRAPH_SPACE,
         spaceAfter=PARAGRAPH_SPACE
-        )
+    )
     )
     Indent_style = deepcopy(styles["Normal"])
     Indent_style.name = 'Indent'
@@ -115,7 +115,8 @@ def compose_pdf(profile):
     p = '<font size=%d>%s %s</font><br/><br/>' % (HEADLINE_FONT_SIZE, profile['firstName'], profile['lastName'])
     p += '%s<br/><br/><br/>' % profile['headline']
     if profile['phoneNumbers']['_total']:
-        p += '<strong>Phone:</strong>&nbsp;&nbsp;&nbsp;&nbsp;<font size=10>%s</font><br/><br/>' % profile['phoneNumbers']['values'][0]['phoneNumber']
+        p += '<strong>Phone:</strong>&nbsp;&nbsp;&nbsp;&nbsp;<font size=10>%s</font><br/><br/>' % profile[
+            'phoneNumbers']['values'][0]['phoneNumber']
     p += '<strong>Email:</strong>&nbsp;&nbsp;&nbsp;&nbsp;<font size=10>%s</font><br/><br/>' % profile['emailAddress']
     p += '<strong>Location:</strong>&nbsp;&nbsp;&nbsp;&nbsp;<font size=10>%s</font><br/><br/>' % profile['location']['name']
     p = Paragraph(p, styles["Normal"])
@@ -127,62 +128,68 @@ def compose_pdf(profile):
     story.append(Paragraph(profile['summary'], styles["Normal"]))
     if profile['positions']['_total']:
         story.append(Paragraph('Work Experience', styles["Headline"]))
-    for position in profile['positions']['values']:
-        position_headline = '%s - %s <font color="%s">|</font> <font size=8>%d/%d - %s</font>' % (
-            position['title'],
-            position['company']['name'],
-            COLOR,
-            position['startDate']['month'],
-            position['startDate']['year'],
-            'present' if position['isCurrent'] else str(position['endDate']['month']) + '/' + str(position['endDate']['year']),
-        )
-        position_summary = position['summary'].replace('\n', '<br/>')
-        story.append(Paragraph(position_headline, styles["CustomBullet"], bulletText='\xe2\x80\xa2'))
-        story.append(Paragraph(position_summary, styles["Indent"]))
+        for position in profile['positions']['values']:
+            position_headline = '%s - %s <font color="%s">|</font> <font size=8>%d/%d - %s</font>' % (
+                position['title'],
+                position['company']['name'],
+                COLOR,
+                position['startDate']['month'],
+                position['startDate']['year'],
+                'present' if position['isCurrent'] else str(
+                    position['endDate']['month']) + '/' + str(position['endDate']['year']),
+            )
+            position_summary = position['summary'].replace('\n', '<br/>')
+            story.append(Paragraph(position_headline, styles["CustomBullet"], bulletText='\xe2\x80\xa2'))
+            story.append(Paragraph(position_summary, styles["Indent"]))
     if profile['educations']['_total']:
         story.append(Paragraph('Education', styles["Headline"]))
-    for education in profile['educations']['values']:
-        education_headline = '%s <font color="%s">|</font> <font size=8>%d - %d</font>' % (
-            education['schoolName'],
-            COLOR,
-            education['startDate']['year'],
-            education['endDate']['year'],
-        )
-        education_headline += '<br/>%s, %s' % (education['degree'], education['fieldOfStudy'])
-        story.append(Paragraph(education_headline, styles["CustomBullet"], bulletText='\xe2\x80\xa2'))
-        for note in education['notes'].split('\n'):
-            story.append(Paragraph(note, styles["Indent"], bulletText='-'))
+        for education in profile['educations']['values']:
+            education_headline = '%s <font color="%s">|</font> <font size=8>%d - %d</font>' % (
+                education['schoolName'],
+                COLOR,
+                education['startDate']['year'],
+                education['endDate']['year'],
+            )
+            education_headline += '<br/>%s, %s' % (education['degree'], education['fieldOfStudy'])
+            story.append(Paragraph(education_headline, styles["CustomBullet"], bulletText='\xe2\x80\xa2'))
+            for note in education['notes'].split('\n'):
+                story.append(Paragraph(note, styles["Indent"], bulletText='-'))
+    if profile['projects']['_total']:
+        story.append(Paragraph('Projects', styles["Headline"]))
+        for project in profile['projects']['values']:
+            story.append(Paragraph(project['name'], styles["CustomBullet"], bulletText='\xe2\x80\xa2'))
+            story.append(Paragraph(project['description'].replace('\n', '<br/>'), styles["Indent"]))
     if profile['skills']['_total']:
         story.append(Paragraph('Skills', styles["Headline"]))
-    p = '  '.join([skill['skill']['name'].upper() for skill in profile['skills']['values']])
-    story.append(Paragraph(p, styles["Skill"]))
+        p = '  '.join([skill['skill']['name'].upper() for skill in profile['skills']['values']])
+        story.append(Paragraph(p, styles["Skill"]))
     if profile['recommendationsReceived']['_total']:
         story.append(Paragraph('Recommendations', styles["Headline"]))
-    for recommendation in profile['recommendationsReceived']['values']:
-        recommendation_headline = '%s %s <font color="%s">|</font> <font size=8>%s</font>' % (
-            recommendation['recommender']['firstName'],
-            recommendation['recommender']['lastName'],
-            COLOR,
-            recommendation['recommendationType']['code'],
-        )
-        recommendation_text = recommendation['recommendationText'].replace('\n', '<br/>')
-        story.append(Paragraph(recommendation_headline, styles["CustomBullet"], bulletText='\xe2\x80\xa2'))
-        story.append(Paragraph(recommendation_text, styles["Indent"]))
+        for recommendation in profile['recommendationsReceived']['values']:
+            recommendation_headline = '%s %s <font color="%s">|</font> <font size=8>%s</font>' % (
+                recommendation['recommender']['firstName'],
+                recommendation['recommender']['lastName'],
+                COLOR,
+                recommendation['recommendationType']['code'],
+            )
+            recommendation_text = recommendation['recommendationText'].replace('\n', '<br/>')
+            story.append(Paragraph(recommendation_headline, styles["CustomBullet"], bulletText='\xe2\x80\xa2'))
+            story.append(Paragraph(recommendation_text, styles["Indent"]))
 
     def onPage(canvas, dcmt):
         canvas.saveState()
         canvas.setFont(FONT_NAME, 7)
         canvas.setStrokeColor(COLOR)
         canvas.setFillColor(COLOR)
-        canvas.drawString(cm, dcmt.pagesize[1] - .75*cm,
-            '%s %s - %s' % (profile['firstName'], profile['lastName'], profile['headline']))
+        canvas.drawString(cm, dcmt.pagesize[1] - .75 * cm,
+                          '%s %s - %s' % (profile['firstName'], profile['lastName'], profile['headline']))
         Local = LocalTimezone()
-        canvas.drawRightString(dcmt.pagesize[0] - cm, dcmt.pagesize[1] - .75*cm,
-            datetime.now(Local).strftime('Generated on %d.%m.%Y %H:%M:%S %Z'))
+        canvas.drawRightString(dcmt.pagesize[0] - cm, dcmt.pagesize[1] - .75 * cm,
+                               datetime.now(Local).strftime('Generated on %d.%m.%Y %H:%M:%S %Z'))
         canvas.line(cm, dcmt.pagesize[1] - cm, dcmt.pagesize[0] - cm, dcmt.pagesize[1] - cm)
-        canvas.line(cm, cm, (dcmt.pagesize[0] - cm)/2.0, cm)
-        canvas.drawCentredString(dcmt.pagesize[0]/2.0, .95*cm, str(dcmt.page))
-        canvas.line((dcmt.pagesize[0] + cm)/2.0, cm, dcmt.pagesize[0] - cm, cm)
+        canvas.line(cm, cm, (dcmt.pagesize[0] - cm) / 2.0, cm)
+        canvas.drawCentredString(dcmt.pagesize[0] / 2.0, .95 * cm, str(dcmt.page))
+        canvas.line((dcmt.pagesize[0] + cm) / 2.0, cm, dcmt.pagesize[0] - cm, cm)
         canvas.restoreState()
 
     doc.build(story, onFirstPage=onPage, onLaterPages=onPage)
